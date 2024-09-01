6bb85fa2414c4dda2adfc24e7b5cdcf4c0af435e
Cece <choush@msudenver.edu>
2024-09-01 16:02:26 -0600

    added socials

--- src/screens/home/HomePage.jsx
+++ src/screens/home/HomePage.jsx
@@ -6,6 +6,8 @@ import myPicture from '../../assets/myPicture.jpg';
 import cloudImage2 from '../../assets/cloud2.png';
 import cloudImage from '../../assets/cloud.png';
 import nej from '../../assets/nej.png';
+import { FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
 
 const HomePage = () => {
     return (
@@ -22,6 +24,12 @@ const HomePage = () => {
                     <div className="profile-picture">
                         <img src={myPicture} alt="My Profile" />
                     </div>
+                    <div className="social-icons">
+                        <a href="https://linkedin.com/in/cecehoush" target="_blank" rel="noopener noreferrer"><FaLinkedin size={26} /></a>
+                        <a href="https://github.com/cecehoush" target="_blank" rel="noopener noreferrer"><FaGithub size={26} /></a>
+                        <a href="https://instagram.com/cetsukii" target="_blank" rel="noopener noreferrer"><FaInstagram size={26} /></a>
+                        <a href="mailto:carolannehoush@gmail.com"><MdEmail size={26} /></a>
+                    </div>
                     <h1>Welcome to My Website!!</h1>
                     <p>Here's a brief summary...</p>
                 </section>
@@ -30,7 +38,7 @@ const HomePage = () => {
 
                 <section id="about" className="full-height-section">
                     <h2>About Me</h2>
-                    <p>This is the about me section...</p>
+                    <p>I am a full stack developer and a UI/UX designer...</p>
                 </section>
 
                 <hr className="border-t border-gray-300 my-8" />
